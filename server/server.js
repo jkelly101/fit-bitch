816c6844a2f6a574a97e90444c8900548733b8ac
kierajcullen <kierajcullen@gmail.com>
2021-05-15 14:21:46 -0400

    heroku

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 require("dotenv").config();
 const express = require("express");
 
@@ -27,7 +28,7 @@ app.use(passport.session());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("../client/build"));
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 // Add routes, both API and view
 app.use(routes);
