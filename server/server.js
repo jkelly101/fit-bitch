f1d42caeeec54f7c8332aef469268e2a1c38c159
kierajcullen <kierajcullen@gmail.com>
2021-05-15 14:03:32 -0400

    heroku mods

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ app.use(passport.session());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("../client/build"));
 }
 // Add routes, both API and view
 app.use(routes);
