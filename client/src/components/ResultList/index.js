9a63ab82f83c8261b73e02c93ad09a3ffa52c961
jkelly101 <jenLkelly@gmail.com>
2021-05-25 19:57:37 -0400

    added width and margins to gif container

--- client/src/components/ResultList/index.js
+++ client/src/components/ResultList/index.js
@@ -3,9 +3,14 @@ import React from "react";
 function ResultList(props) {
   return (
     <ul className="list-group">
-      {props.results.map(result => (
-        <li className="list-group-item" key={result.id}>
-          <img alt={result.title} className="img-fluid" src={result.images.original.url} />
+      {props.results.map((result) => (
+        <li className="list-group-item m-auto" key={result.id}>
+          <img
+            alt={result.title}
+            className="img-fluid"
+            src={result.images.original.url}
+            width="800px"
+          />
         </li>
       ))}
     </ul>
